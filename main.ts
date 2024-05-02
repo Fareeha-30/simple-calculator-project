5cab89cd52dda79f76dcabe4b8dd4142051900cb
Fareeha-30 <fareeha.saleem30@live.com>
2024-05-03 01:00:37 +0500

    Adding a simple calculator

---
+++ main.ts
@@ -0,0 +1,33 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+    {message: "enter first number", type: "number", name: "firstNumber" },
+    {message: "enter second number", type: "number", name: "secondNumber" },
+{
+    message: "select one of the operators to perform operation",
+    type: "list",
+    name: "operator",
+    choices:["Addition","Subtraction","Multiplication","Division"]
+},
+]);
+//conditional statment
+// if (answer.operator === "Addition") {
+// console.log("your value is:" + answer.firstNumber + answer.secondNumber);}
+//adding as a string because " " adding as string so answer will be 10 +20 =1020
+//console.log(answer);
+
+if (answer.operator === "Addition" ) {
+console.log(answer.firstNumber + answer.secondNumber);}
+
+else if(answer.operator === "Subtraction" ) {
+console.log(answer.firstNumber - answer.secondNumber);}
+
+else if(answer.operator === "Multiplication" ) {
+console.log(answer.firstNumber * answer.secondNumber);}
+
+else if(answer.operator === "Divsion" ) {
+console.log(answer.firstNumber / answer.secondNumber);}
+
+else { console.log("Please select valid operator")}
